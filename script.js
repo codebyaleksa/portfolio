2bce59be320e9d398e27e4516d37028ff52bd1f2
Aleksa <aleksadimitrijevic515@gmail.com>
2025-07-15 02:39:16 +0200

    Spojeno sa remote

--- script.js
+++ script.js
@@ -276,6 +276,17 @@ menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+const overlay = document.getElementById("overlay");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  overlay.classList.remove("active");
+});
 
 
 
